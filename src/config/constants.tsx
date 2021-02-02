ed4ecb12f1458980e8be08b9884dcb1d799d5faf
Oscar Gracia <oscar.graciae@gmail.com>
2021-02-01 21:35:22 -0600

    Deploy prueba

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -1,5 +1,6 @@
 // export const API_DOMAIN =  process.browser ? 'http://localhost:8001' : 'http://api:8001'
-export const API_DOMAIN = 'http://localhost:8001';
+// export const API_DOMAIN = 'http://localhost:8001';
+export const API_DOMAIN = 'http://http://3.135.239.2:8001';
 // export const API_DOMAIN = 'http://api:8001'
 export const URL_API = `${API_DOMAIN}/api/v1`;
 export const APP_NAME = 'Reserly';
