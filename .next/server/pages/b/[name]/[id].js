03edcfb132b9e546e748b485d54d0d2cdc9c98bf
Oscar Gracia <oscar.graciae@gmail.com>
2021-02-01 21:39:09 -0600

    Deploy prueba

--- .next/server/pages/b/[name]/[id].js
+++ .next/server/pages/b/[name]/[id].js
@@ -341,7 +341,7 @@ const formatDate = date => {
 /* unused harmony export TOKEN_MAPBOX */
 // export const API_DOMAIN =  process.browser ? 'http://localhost:8001' : 'http://api:8001'
 // export const API_DOMAIN = 'http://localhost:8001';
-const API_DOMAIN = 'http://http://3.135.239.2:8001'; // export const API_DOMAIN = 'http://api:8001'
+const API_DOMAIN = 'http://3.135.239.2:8001'; // export const API_DOMAIN = 'http://api:8001'
 
 const URL_API = `${API_DOMAIN}/api/v1`;
 const APP_NAME = 'Reserly';
