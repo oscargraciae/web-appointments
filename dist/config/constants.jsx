d634b6de88f672b1102efb8099e275e43b4f5e0a
Oscar Gracia <oscar.graciae@gmail.com>
2021-02-02 18:48:36 -0600

    Deploy a producción

--- dist/config/constants.jsx
+++ dist/config/constants.jsx
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TOKEN_MAPBOX = exports.APP_NAME = exports.URL_API = exports.API_DOMAIN = void 0;
 // export const API_DOMAIN =  process.browser ? 'http://localhost:8001' : 'http://api:8001'
 // export const API_DOMAIN = 'http://localhost:8001';
-exports.API_DOMAIN = 'http://3.135.239.2:8001';
+exports.API_DOMAIN = 'https://api.reserly';
 // export const API_DOMAIN = 'http://api:8001'
 exports.URL_API = exports.API_DOMAIN + "/api/v1";
 exports.APP_NAME = 'Reserly';
